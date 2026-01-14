77aa3dacacd6b02309b5120ed79d445d50559c69
unknown <cedricbaran622@gmail.com>
2026-01-14 18:35:36 +0800

    first commit

---
+++ src/main/java/mine/java_mods/chezzie/ChezzieDay.java
@@ -0,0 +1,18 @@
+package mine.java_mods.chezzie;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ChezzieDay implements ModInitializer {
+
+    public static final String MOD_ID = "chezzie-day";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/mine/java_mods/chezzie/ChezzieDayModClient.java
@@ -0,0 +1,10 @@
+package mine.java_mods.chezzie;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChezzieDayModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
